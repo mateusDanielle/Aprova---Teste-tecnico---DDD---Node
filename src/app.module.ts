7f9d76e2180485c5a7487c7eb74dd72795986142
Mateus Danielle <mateusdanielle@edu.fecap.br>
2025-08-06 15:39:53 -0300

    🧹 chore: remove unnecessary AppController and AppService files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AppConfigModule } from './config';
 import { DatabaseModule } from './infrastructure/database/database.module';
 import { ApplicationModule } from './application/application.module';
@@ -13,7 +11,5 @@ import { PresentationModule } from './presentation/presentation.module';
     ApplicationModule,
     PresentationModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
